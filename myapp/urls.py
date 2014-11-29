4ae168d1ced13bbb68ddbd5cb29f278fa8c12b75
POD666 <pod666@i.ua>
2014-11-30 00:51:08 +0200

    api fix

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import patterns, include, url
-from views import *
+from api import *
 
 urlpatterns = patterns('',
-    url(r'^/', include('myapp.api_urls'))
+    url(r'^$', apimap),
+
+    url(r'^seed/$', seed),
+
+    url(r'^country/$', country_all),
+    url(r'^country/(?P<code>\w+)/$', country_by_code),
+
+    url(r'^university/$', university_all),
+    url(r'^university/(?P<code>\w+)/$', university_by_code),
 )

02e38db62f97e6e2e590da4a66092e1d34bf6502
POD666 <pod666@i.ua>
2014-11-30 00:26:29 +0200

    x3

--- project/urls.py
+++ project/urls.py
@@ -7,17 +7,17 @@ from views import *
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'django.contrib.staticfiles.views.serve', kwargs={'path': 'index.html', 'document_root': settings.STATIC_ROOT}),
-    url(r'^.*$', RedirectView.as_view(url='/', permanent=False), name='index'),
+    url(r'^$', home),
 
     url(r'^', include('myapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^api/profile/$', profile),
-    url(r'^api/logout/$', logout),
-    url(r'^api/complete/(?P<backend>[^/]+)/$', AuthComplete.as_view()),
-    url(r'^api/login-error/$', LoginError.as_view()),
-    url(r'^api/', include('social_auth.urls')),
+    url(r'^/profile/$', profile),
+    url(r'^/logout/$', logout),
+    url(r'^/complete/(?P<backend>[^/]+)/$', AuthComplete.as_view()),
+    url(r'^/login-error/$', LoginError.as_view()),
+    url(r'^', include('social_auth.urls')),
 
 
+    #url(r'^.*$', RedirectView.as_view(url='/', permanent=False), name='index'),
 )
